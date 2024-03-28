f3a59875577dee594ed81d2d02b257001d2fd10a
sayo-2123 <classa1990@gmail.com>
2024-03-28 19:53:25 +0900

    課題1実装

---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default Headers = () => {
+  return (
+    <header className="header">
+      <div className="siteName">Blog</div>
+      <div className="toSupportPage">お問い合わせ</div>
+    </header>
+  );
+};
